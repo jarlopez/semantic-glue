d6e7ba5dc3ca21a809a1234f8039d50d7d4538eb
Johan Mickos <jmickos@bu.edu>
2017-03-05 11:36:55 +0100

    Add more primitive types to util

--- src/main/java/edu/kth/wsglue/parsing/util/WSDLUtil.java
+++ src/main/java/edu/kth/wsglue/parsing/util/WSDLUtil.java
@@ -7,10 +7,33 @@ import java.util.Set;
 public class WSDLUtil {
     public static final Set<String> primitiveTypes = new HashSet<>();
 
-    private static final String PRIMITIVE_TYPE_NAMES = "boolean decimal int short long double string date dateTime Array";
-
+    private static final String PT_DELIM = "\r\n";
+    private static final String PRIMITIVE_TYPE_NAMES = "" +
+            "anyType" + PT_DELIM +
+            "anyURI" + PT_DELIM +
+            "Array" + PT_DELIM +
+            "base64Binary" + PT_DELIM +
+            "boolean" + PT_DELIM +
+            "byte" + PT_DELIM +
+            "date" + PT_DELIM +
+            "dateTime" + PT_DELIM +
+            "decimal" + PT_DELIM +
+            "double" + PT_DELIM +
+            "float" + PT_DELIM +
+            "ID" + PT_DELIM +
+            "IDREF" + PT_DELIM +
+            "int" + PT_DELIM +
+            "integer" + PT_DELIM +
+            "long" + PT_DELIM +
+            "QName" + PT_DELIM +
+            "short" + PT_DELIM +
+            "string" + PT_DELIM +
+            "unsignedByte" + PT_DELIM +
+            "unsignedInt" + PT_DELIM +
+            "unsignedLong" + PT_DELIM +
+            "unsignedShort";
     static {
-        primitiveTypes.addAll(Arrays.asList(PRIMITIVE_TYPE_NAMES.split(" ")));
+        primitiveTypes.addAll(Arrays.asList(PRIMITIVE_TYPE_NAMES.split(PT_DELIM)));
     }
 
     public static boolean isPrimitiveType(String test) {
